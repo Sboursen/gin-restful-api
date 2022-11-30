84fb36354d6bc3e45d5d2dfdabdcd88c005d0a80
Soufiane Boursen <dev.sboursen@gmail.com>
2022-11-30 20:04:00 +0100

    Handle get article by id request

--- main.go
+++ main.go
@@ -24,8 +24,8 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
-
 	router.Run("localhost:8080")
 }
 
@@ -43,4 +43,16 @@ func postAlbums(c *gin.Context) {
 	}
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
